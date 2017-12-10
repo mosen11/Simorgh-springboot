2a245d6eca6c94ddf01c5168b4d21b2bb4f57f7b
Mosen Shoushteri <mosen.shousteri@kadaster.nl>
2017-12-10 20:18:11 +0100

    Second commit

--- src/main/java/nl/simorgh/app/jms/Receiver.java
+++ src/main/java/nl/simorgh/app/jms/Receiver.java
@@ -8,6 +8,7 @@ import javax.jms.TextMessage;
 
 @Component
 public class Receiver {
+
 	@JmsListener(destination = "mailbox", containerFactory = "myFactory")
 	public void receiveMessage(TextMessage email) throws JMSException {
 		System.out.println("Received <" + email.getText() + ">");
